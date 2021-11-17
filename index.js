a9d857cea9867ce75694f7f5258fa7062c3ee5ef
Brett Logan <lindluni@github.com>
2021-11-16 23:21:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core')
+const {Octokit} = require("@octokit/rest")
+const {retry} = require("@octokit/plugin-retry");
+const {throttling} = require("@octokit/plugin-throttling");
+
+const _Octokit = Octokit.plugin(retry, throttling);
+
+(async function main() {
+    const org = core.getInput('org', {required: true, trimWhitespace: true})
+    const repo = core.getInput('repo', {required: true, trimWhitespace: true})
+    const workflowID = core.getInput('workflow_id', {required: true, trimWhitespace: true})
+    const sha = core.getInput('sha', {required: true, trimWhitespace: true})
+    const token = core.getInput('token', {required: true, trimWhitespace: true})
+    const client = new _Octokit({
+        auth: token,
+        request: {
+            retries: 100
+        },
+        throttle: {
+            onRateLimit: (retryAfter, options, octokit) => {
+                octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
+                if (options.request.retryCount === 0) {
+                    octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+                    return true;
+                }
+            },
+            onAbuseLimit: (retryAfter, options, octokit) => {
+                octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`);
+            },
+        }
+    });
+    const jobs = await client.paginate(client.actions.listWorkflowRuns, {
+        owner: org,
+        repo: repo,
+        workflow_id: workflowID,
+    })
+    for (const job of jobs) {
+        if (job.head_sha === sha) {
+            core.info('Found job')
+            core.setOutput('job-id', job.id)
+            return
+        }
+    }
+})()
